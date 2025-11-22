cadf1fe2da4a3a7739832ce10e1a4fd1c9ff9043
javieradel <ashair455@gmail.com>
2025-11-22 16:04:16 -0300

    INITIAL_COMMIT

---
+++ src/main/java/com/talento_tech/prueba_final/PruebaFinalApplication.java
@@ -0,0 +1,12 @@
+package com.talento_tech.prueba_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaFinalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/talento_tech/prueba_final/PruebaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talento_tech.prueba_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
